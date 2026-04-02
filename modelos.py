a05770f5460c9fa7a3b90cb1e848ad77ba946c18
ArthurToso <arthurtoso2003@gmail.com>
2026-04-02 14:44:13 -0300

    fix(modelos.py): arruma description dos atributos e adiciona breve explicacao pydantic

--- modelos.py
+++ modelos.py
@@ -1,3 +1,7 @@
+# O Pydantic é usado para garantir a validação do tipo do dado de uma classe
+# Ao herdar a  classe BaseModel, a classe vira um modelo validado do pydantic
+# por exemplo, se o atributo for idade: int e tentar passar idade = "vinte", vai retornar erro usando o pydantic
+# Por python base não ocorreria erro 
 from pydantic import BaseModel, Field
 from typing import List
 
@@ -6,13 +10,24 @@ class InformacoesExtraidas(BaseModel):
     Contrato de dados que o agente extrator deve preencher.
     """
     datas: List[str] = Field(
-        description= "Datas, preservando o formato exato como aparecem no texto (ex: '31 de março de 2024', '07/03/2024', 'Q1 2024')"
+        description=( 
+            "Lista de todas as datas encontradas no documento, "
+            "preservando o formato exato como aparecem no texto (ex: '31 de março de 2024', '07/03/2024', 'Q1 2024')"
+            "Seja exaustivo: Não omita nenhuma ocorrência"
+        )
     )
 
     valores_monetarios: List[str] = Field(
-        description="Valores monetarios, inlcuindo a moeda e preservando o formato exato como aparecem no texto (ex:  'R$ 214.750.320,00', 'USD 12.500.000', 'R$ 1,58 por ação')"
+        description=("Lista de todos os Valores monetarios encontrados no texto, "
+        "inlcuindo a moeda e preservando o formato exato como aparecem no texto (ex:  'R$ 214.750.320,00', 'USD 12.500.000', 'R$ 1,58 por ação')"
+        "Seja exaustivo: Não omita nenhuma ocorrência"
+        )
     )
 
     entidades: List[str] = Field(
-        description="Pessoas fisicas com cargo quando disponivel, e tambem empresas/organizacoes (ex.: 'Dr. Renato Cavalcante Drummond (CEO)', 'Tecnoverde Sistemas Ltda.')"
+        description=(
+            "Lista de todas as Pessoas fisicas com cargo quando disponivel encontradas no texto,"
+            " e tambem empresas/organizacoes (ex.: 'Dr. Renato Cavalcante Drummond (CEO)', 'Tecnoverde Sistemas Ltda.')"
+            "Seja exaustivo: Não omita nenhuma ocorrência"
+        )
     )
